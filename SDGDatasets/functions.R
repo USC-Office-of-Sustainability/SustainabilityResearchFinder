b9df43fe662b62fb0bb6334642af3a9254470332
Alison <alisonchen.y@gmail.com>
2022-10-03 15:45:11 -0700

    takes all csv in /DownloadedData/ and formats them into two column csv in /FormattedData/

---
+++ SDGDatasets/functions.R
@@ -0,0 +1,36 @@
+# downloaded csv goes into DownloadedData folder
+# newly formatted csv will be found in FormattedData folder
+
+# paths of csv files
+ff = list.files("./DownloadedData", pattern = "csv", full.names = TRUE)
+
+reformat = function(filename) {
+  # read csv
+  d = read.csv(filename)
+  
+  # clean abstract
+  # remove copyright
+  d$Abstract = gsub("([^.]*©.*)", "", d$Abstract)
+
+  # combine into a new dataframe with one column for SDG and one column for text
+  # assuming that the file name is in the format with SDG#
+  relativefilename = gsub(".*\\/", "", filename)
+  sdgnum = gsub("[0-9]*SDG([0-9]*)\\.csv", "\\1", relativefilename)
+  
+  combinedtext = ""
+  if ("Author.Keywords" %in% colnames(d)) {
+    combinedtext = paste("[TITLE]", d$Titles, "[KEYWORDS]", d$Author.Keywords, "[ABSTRACT]", d$Abstract)
+  } else {
+    combinedtext = paste("[TITLE]", d$Titles, "[ABSTRACT]", d$Abstract)
+  }
+  
+  df = data.frame("SDG" = sdgnum, "Text" = combinedtext)
+  
+  # output csv
+  newfilename = paste("./FormattedData/re", relativefilename, sep = "")
+  write.csv(df, file = newfilename, row.names = FALSE)
+  
+  df
+}
+
+tmp = lapply(ff, reformat)
