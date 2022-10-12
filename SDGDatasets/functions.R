f7156c452eacd5b5daa0971d18f90c6aa877c3f8
Alison <alisonchen.y@gmail.com>
2022-10-11 17:36:22 -0700

    refactored the code: cleanText and oneString functions

--- SDGDatasets/functions.R
+++ SDGDatasets/functions.R
@@ -9,28 +9,46 @@ removePunctuation = function(x) {
   gsub("[^[:alnum:][:space:]-]", "", x)
 }
 
+cleanText = function(d) {
+  # remove copyright
+  d$Abstract = gsub("([^.]*©.*)", "", d$Abstract)
+  
+  # remove punctuation
+  if ("Titles" %in% colnames(d)) {
+    d$Titles = removePunctuation(d$Titles)
+  } else {
+    d$Title = removePunctuation(d$Title)
+  }
+  d$Abstract = removePunctuation(d$Abstract)
+  if ("Author.Keywords" %in% colnames(d)) {
+    d$Author.Keywords = removePunctuation(d$Author.Keywords)
+  }
+  d
+}
+
+# input: 1 data frame
+# output: string in the format "[TITLE]...[KEYWORDS]...[ABSTRACT]..."
+oneString = function(d) {
+  if ("Author.Keywords" %in% colnames(d)) {
+    return (paste("[TITLE]", d$Titles, "[KEYWORDS]", d$Author.Keywords, "[ABSTRACT]", d$Abstract))
+  } else {
+    return (paste("[TITLE]", d$Titles, "[ABSTRACT]", d$Abstract))
+  }
+}
+
 reformat = function(filename) {
   # read csv
   d = read.csv(filename)
   
-  # clean abstract
-  # remove copyright
-  d$Abstract = gsub("([^.]*©.*)", "", d$Abstract)
-  # remove punctuation
-  d$Titles = removePunctuation(d$Titles)
-  d$Abstract = removePunctuation(d$Abstract)
+  # clean text
+  d = cleanText(d)
 
   # combine into a new dataframe with one column for SDG and one column for text
   # assuming that the file name is in the format with SDG#
   relativefilename = gsub(".*\\/", "", filename)
   sdgnum = gsub("[0-9]*SDG([0-9]*)\\.csv", "\\1", relativefilename)
   
-  combinedtext = ""
-  if ("Author.Keywords" %in% colnames(d)) {
-    combinedtext = paste("[TITLE]", d$Titles, "[KEYWORDS]", d$Author.Keywords, "[ABSTRACT]", d$Abstract)
-  } else {
-    combinedtext = paste("[TITLE]", d$Titles, "[ABSTRACT]", d$Abstract)
-  }
+  combinedtext = oneString(d)
   
   df = data.frame("SDG" = sdgnum, "Text" = combinedtext)
   
@@ -48,20 +66,14 @@ rm(tmp)
 
 # for testing data
 d = read.csv("./DownloadedTestingData/SDG_PUB_testing_ckidsdataset.csv")
-# clean abstract
-# remove copyright
-d$Abstract = gsub("([^.]*©.*)", "", d$Abstract)
-# remove punctuation
-d$Title = removePunctuation(d$Title)
-d$Abstract = removePunctuation(d$Abstract)
-d$Author.Keywords = removePunctuation(d$Author.Keywords)
+d = cleanText(d)
 
 # remove SDG 0 and 17
 d = d[d$Primary.SDG %in% seq(1,16),]
 
-combinedtext = paste("[TITLE]", d$Title, "[KEYWORDS]", d$Author.Keywords, "[ABSTRACT]", d$Abstract)
+combinedtext = oneString(d)
 
 df = data.frame("SDG" = d$Primary.SDG, "Text" = combinedtext)
 
-newfilename = "./FormattedData/SDG_PUB_Testing"
+newfilename = "./FormattedData/SDG_PUB_Testing.csv"
 write.csv(df, file = newfilename, row.names = FALSE)
