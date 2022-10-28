9cc6628f12dfade1604dde9298b3ecb95ab036e2
Alison <alisonchen.y@gmail.com>
2022-10-28 11:50:59 -0700

    extract usc authors from 4067 publications but seem to be missing 230 publications

--- SDGDatasets/usc_dataframe.R
+++ SDGDatasets/usc_dataframe.R
@@ -9,20 +9,20 @@ write.csv(all, file = "./USC_all.csv", row.names = FALSE)
 
 # many to many relationship btwn publication and authors
 # publication table, publicationauthor bridge table, author table
-publication_author = data.frame()
+publication_author = data.frame() # DOI and AuthorID
 author = data.frame(matrix(ncol=2,nrow=0, dimnames=list(NULL, c("ID", "Name"))))
 
-auth_affl_vec = strsplit(b$Authors.with.affiliations[1], ";")[[1]]
-matched = grep("university of southern california", auth_affl_vec, ignore.case = TRUE)
-# for each match split by , 
-# name is first entry
-for (i in matched) {
-  name = trimws(strsplit(auth_affl_vec[i], ",")[[1]][1])
-  authID = trimws(strsplit(b$Author.s..ID[1], ";")[[1]][i])
-  publication_author = rbind(publication_author, data.frame(DOI = b$DOI[1], AuthorId = authID))
-  if (!(authID %in% author$ID)) {
-    author = rbind(author, data.frame("ID" = authID, "Name" = name))
+for (j in 1:nrow(b)) {
+  auth_affl_vec = strsplit(b$Authors.with.affiliations[j], ";")[[1]]
+  matched = grep("university of southern california", auth_affl_vec, ignore.case = TRUE)
+  # for each match split by , 
+  # name is first entry
+  for (i in matched) {
+    name = trimws(strsplit(auth_affl_vec[i], ",")[[1]][1])
+    authorID = trimws(strsplit(b$Author.s..ID[j], ";")[[1]][i])
+    publication_author = rbind(publication_author, data.frame(DOI = b$DOI[j], AuthorId = authorID))
+    if (!(authorID %in% author$ID)) {
+      author = rbind(author, data.frame("ID" = authorID, "Name" = name))
+    }
   }
-  
 }
-
