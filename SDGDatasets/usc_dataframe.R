5a1fccb847cdb3dd76a9e8c4da8b4b383ea8730b
Alison <alisonchen.y@gmail.com>
2022-11-10 23:28:06 -0800

    searched up 1 person in USC directory

--- SDGDatasets/usc_dataframe.R
+++ SDGDatasets/usc_dataframe.R
@@ -25,20 +25,51 @@ author = data.frame(matrix(ncol=2,nrow=0, dimnames=list(NULL, c("ID", "Name"))))
 for (j in 1:10) {
   auth_affl_vec = strsplit(tmp$Authors.with.affiliations[j], ";")[[1]]
   matched = grep("(uni[versity\\.]{0,} (of )?sou?th[ernm]{0,}[,-]? ?california)|(USC)|(keck school of medicine)|(keck medical center)|(keck medical school)", auth_affl_vec, ignore.case = TRUE)
+  auth_full_name = strsplit(tmp$Author.full.names[j], ";")[[1]]
   # for each match split by , 
   # name is first entry
   for (i in matched) {
     name = trimws(strsplit(auth_affl_vec[i], ",")[[1]][1])
+    lastname = strsplit(name, " ")[[1]][1]
+    firstname = strsplit(trimws(strsplit(auth_full_name[i], ",")[[1]][2]), " ")[[1]][1]
     authorID = trimws(strsplit(tmp$Author.s..ID[j], ";")[[1]][i])
     publication_author = rbind(publication_author, data.frame(DOI = tmp$DOI[j], AuthorId = authorID))
     if (!(authorID %in% author$ID)) {
-      author = rbind(author, data.frame("ID" = authorID, "Name" = name))
+      author = rbind(author, data.frame("ID" = authorID, "Name" = name, "FName" = firstname, "LName" = lastname))
     }
   }
 }
 
 # check which(!(tmp$DOI %in% publication_author$DOI))
 # tmp$Authors.with.affiliations[9250]
+
+# USC directory
+library(RCurl)
+library(RJSONIO)
+con = getCurlHandle(followlocation = TRUE, verbose = TRUE,ssl.verifypeer = FALSE, cookie = "mbox=session#cf1541a039224330a7690b36e8632037#1663617437|PC#cf1541a039224330a7690b36e8632037.35_0#1726860377; AMCV_4D6368F454EC41940A4C98A6@AdobeOrg=1176715910|MCIDTS|19255|MCMID|81731551737931773397180553428496293094|MCAID|NONE|MCOPTOUT-1663622785s|NONE|vVersion|5.4.0; AMCV_A450776A5245ACC00A490D44@AdobeOrg=-1124106680|MCIDTS|19269|MCMID|36216961312940415734330619965701752051|MCAID|NONE|MCOPTOUT-1664835445s|NONE|vVersion|5.2.0; WWTRBQJP=0297a45327-1bed-4feP8LPIjg7QJrxyxTcPqkTtMYHx66rtBXzG3rtRfi8z0ixdXzFE1sGBteOer3oArIFIY")
+tt = getForm("https://uscdirectory.usc.edu/web/directory/faculty-staff/proxy.php", 
+             first = "lynne",
+             last = "casper", 
+             curl = con,
+             .opts = list(httpheader = c("Accept" = "text/plain, */*; q=0.01",
+                                         "Accept-Encoding" = "gzip, deflate, br",
+                                         "Accept-Language" = "en-US,en;q=0.9",
+                                         "Cache-Control" = "no-cache",
+                                         "Connection" = "keep-alive",
+                                         "Host" = "uscdirectory.usc.edu",
+                                         "Pragma" = "no-cache",
+                                         "Referer" = "https://uscdirectory.usc.edu/web/directory/faculty-staff/",
+                                           "sec-ch-ua" = '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+                                         "sec-ch-ua-mobile" = "?1",
+                                         "sec-ch-ua-platform" = "Android",
+                                         "Sec-Fetch-Dest" ="empty",
+                                         "Sec-Fetch-Mode" = "cors",
+                                         "Sec-Fetch-Site" = "same-origin",
+                                         "User-Agent" = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+                                         "X-Requested-With" = "XMLHttpRequest")))
+vals = fromJSON(tt)
+
+# SCOPUS
 
 api_key = Sys.getenv('Elsevier_API')
 
