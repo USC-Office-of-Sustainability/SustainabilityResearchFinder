0c065ef538dc64a131c119f7c15f1e2339ba39ea
chenalison <alisonchen.y@gmail.com>
2022-11-28 14:23:55 -0800

    initial rshiny dashboard

---
+++ RShinyDashboard/pub_app.R
@@ -0,0 +1,102 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+# install.packages("plotly")
+library(plotly)
+# install.packages("wordcloud")
+library(wordcloud)
+# install.packages("DT")
+library(DT)
+# install.packages("ggplot2")
+library(ggplot2)
+# install.packages("ggrepel")
+library(ggrepel)
+
+library(shiny)
+
+publications = read.csv("USC_SDG1to16.csv")
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage( skin="black",
+                     
+                     # Application title
+                     dashboardHeader(title = "USC SDG Mapping"),
+                     
+                     
+                     dashboardSidebar(
+                       sidebarMenu( #will eventually add Schools to sdgs and sdgs to schools
+                         menuItem("Home (About)", tabName = "6"), #Ric
+                         menuItem("Learn About The SDGs", tabName = "5"),#Bhavya
+                         menuItem("USC Research: SDGs by Year", tabName = "4"),#Ric
+                         menuItem("USC Research: SDGs by Department", tabName = "3"), #Xinyi
+                         menuItem("View USC Scholars and Departments by SDGs", tabName = "2"), #Aurora
+                         menuItem("Find SDGs and Publications by USC Author", tabName = "1") #Alison
+                       )
+                     ),
+                     dashboardBody( tags$head(tags$link(rel="stylesheet", type="text/css", href="custom.css")), #link up css stylesheet
+                                    tabItems(tabItem(tabName = "2",
+                                                     fluidPage(
+                                                       h1("USC Research: SDGs By Year"),
+                                                       #h3("this is a description"),
+                                                       div(style="font-size:24px;",selectInput(inputId = "Year",
+                                                                                               label = "Choose Year",
+                                                                                               # selected = "AY19",
+                                                                                               choices = sort(unique(publications$Year)))),
+                                                      
+                                                       # h3("Not including multiple sections"),
+                                                       # plotOutput("pie1"), 
+                                                       # textOutput("pie1_numbers")
+                                                       
+                                                       # fluidRow(
+                                                       #   h3("Including multiple sections"),
+                                                       #   plotOutput("pie2"),
+                                                       #   textOutput("pie2_numbers")
+                                                       # ),
+                                                       # fluidRow(
+                                                       #   h3("By department"),
+                                                       #   plotOutput("pie3"),
+                                                       #   textOutput("pie3_numbers")
+                                                       # ),
+                                                      
+                                                       h3("Yearly Total Count of Publications By SDG"),
+                                                       fluidRow(column(6, plotOutput("year_sdg_barplot"))),
+                                                    
+                                                     ) # end fluid page
+                                    )
+                                                     )))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$year_sdg_barplot <- renderPlot(
+    width = 800,
+    height = 550,{
+    year_sdg_barplot <- publications %>%
+      filter(Year == input$Year) %>%
+      count(Year,Primary.SDG ) %>%
+      mutate(Freq = n) %>%
+      ggplot(aes(x = Primary.SDG,y=Freq, fill = factor(as.numeric(Primary.SDG)))) +
+      geom_col() +
+      geom_text(aes(label = Freq), vjust = -0.2) +
+      # geom_hline(yintercept = c(10, 15), color = c("#ffc33c", "#00bc9e")) +
+      labs(title = paste0(" (", input$Year, ") ", "Count of Publications Per SDG"),
+           fill = "SDG",
+           x = "SDG",
+           y = "Count of Publications") +
+      guides(alpha = FALSE) +
+      theme(text = element_text(size = 18)) #+
+      #scale_fill_manual(values = sdg_class_keyword_colors)
+    
+    return(year_sdg_barplot)
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
