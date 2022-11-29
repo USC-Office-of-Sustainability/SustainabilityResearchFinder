e9de20cf203c53b9a4c7773aeb9d34e34d0b09fd
chenalison <alisonchen.y@gmail.com>
2022-11-28 20:23:18 -0800

    added Ric's code

--- RShinyDashboard/pub_app.R
+++ RShinyDashboard/pub_app.R
@@ -25,6 +25,7 @@ publications = read.csv("USC_SDG1to16.csv")
 authors = read.csv("USCauthorsSDG1to16.csv")
 authorChoices = setNames(authors$ID, authors$Name)
 pub_auth = read.csv("USCpubauthfullinfoSDG1to16.csv")
+pubwithzero = read.csv("USC_SDG0to16.csv")
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage( skin="black",
@@ -44,35 +45,43 @@ ui <- dashboardPage( skin="black",
                        )
                      ),
                      dashboardBody( tags$head(tags$link(rel="stylesheet", type="text/css", href="custom.css")), #link up css stylesheet
-                                    tabItems(tabItem(tabName = "2",
-                                                     fluidPage(
-                                                       h1("USC Research: SDGs By Year"),
-                                                       #h3("this is a description"),
-                                                       div(style="font-size:24px;",selectInput(inputId = "Year",
-                                                                                               label = "Choose Year",
-                                                                                               # selected = "AY19",
-                                                                                               choices = sort(unique(publications$Year)))),
-                                                       
-                                                       # h3("Not including multiple sections"),
-                                                       # plotOutput("pie1"), 
-                                                       # textOutput("pie1_numbers")
-                                                       
-                                                       # fluidRow(
-                                                       #   h3("Including multiple sections"),
-                                                       #   plotOutput("pie2"),
-                                                       #   textOutput("pie2_numbers")
-                                                       # ),
-                                                       # fluidRow(
-                                                       #   h3("By department"),
-                                                       #   plotOutput("pie3"),
-                                                       #   textOutput("pie3_numbers")
-                                                       # ),
-                                                       
-                                                       h3("Yearly Total Count of Publications By SDG"),
-                                                       fluidRow(column(6, plotOutput("year_sdg_barplot"))),
-                                                       
-                                                     ) # end fluid page
-                                    ), # end tab item
+                                    tabItems(
+                                      tabItem(tabName = "6",
+                                              fluidPage(
+                                                h1("Home (Project Overview)"),
+                                                # fluidRow(
+                                                h3(strong("Are you interested in sustainability and the ",
+                                                          a("UN Sustainability Development Goals (SDGs)?", href="https://sdgs.un.org")), 
+                                                   "If so, you have come to the right place! 
+Right now, the results are from Scopus SDG Search Query. We are working on updating the dashboard with more accurate SDG classification using Machine Learning.
+",br(), br(),strong("This 
+                           dashboard is a tool that enables you to see which research publications at USC 
+                           relate to the 16 UN SDGs (SDG 17 is not included for now). You can use this dashboard as a tool to find your authors and publications that match your academic interest!"),
+                                                   br(),br(),"Sustainability incorporates protection for the environment, 
+                           balancing a growing economy, and social responsibility to lead to an 
+                           improved quality of life for current and future generations. Here, 
+                           we have created a program in collaboration with Carnegie Mellon University 
+                           to elevate awareness of sustainability in higher education." ),
+                                                
+                                                
+                                                
+                                                fluidRow(img(src="Asgmt_Earth_Research.png", height="550", style="display: block; margin-left: auto; margin-right: auto;"))
+                                              )
+                                      ), # end tab item 6
+                                      tabItem(tabName = "4",
+                                              fluidPage(
+                                                h1("USC Research: SDGs By Year"),
+                                                #h3("this is a description"),
+                                                div(style="font-size:24px;",selectInput(inputId = "Year",
+                                                                                        label = "Choose Year", choices = sort(unique(publications$Year)))), h3("Yearly Total Count of Publications By SDG"), 
+                                                fluidRow(column(6, plotOutput("year_sdg_barplot"))),
+                                                #h3("SDG Related Research vs. Non-related Research"),
+                                                #fluidRow(column(12, plotOutput("pie1")))
+                                                
+                                                fluidRow(h3("SDG Related Research vs. Non-related Research"),
+                                                         plotOutput("pie1"),
+                                                         
+                                                ))), # end tab item
                                     tabItem(tabName = "1",
                                             fluidPage(
                                               h1("Find SDGs and Publications by USC Author"),
@@ -98,8 +107,8 @@ ui <- dashboardPage( skin="black",
 server <- function(input, output) {
   
   output$year_sdg_barplot <- renderPlot(
-    width = 800,
-    height = 550,{
+    width = 600,
+    height = 400,{
       year_sdg_barplot <- publications %>%
         filter(Year == input$Year) %>%
         count(Year,Primary.SDG ) %>%
@@ -107,17 +116,46 @@ server <- function(input, output) {
         ggplot(aes(x = Primary.SDG,y=Freq, fill = factor(as.numeric(Primary.SDG)))) +
         geom_col() +
         geom_text(aes(label = Freq), vjust = -0.2) +
-        # geom_hline(yintercept = c(10, 15), color = c("#ffc33c", "#00bc9e")) +
         labs(title = paste0(" (", input$Year, ") ", "Count of Publications Per SDG"),
              fill = "SDG",
              x = "SDG",
              y = "Count of Publications") +
         guides(alpha = FALSE) +
-        theme(text = element_text(size = 18)) #+
-      #scale_fill_manual(values = sdg_class_keyword_colors)
-      
+        theme(text = element_text(size = 18)) 
       return(year_sdg_barplot)
     })
+  
+  output$pie1 <- renderPlot({
+    pie_data <- pubwithzero %>% filter(Year %in% input$Year) 
+    sum_notrelated = sum(is.na(pie_data$Primary.SDG))
+    sum_focused = sum(!is.na(pie_data$Primary.SDG))
+    vals=c(sum_notrelated, sum_focused)
+    labels=c("Not Related", "Related")
+    pie = data.frame(labels, vals)
+    pie = data.frame(labels, vals)
+    pie1 <- pie %>% 
+      mutate(csum = rev(cumsum(rev(vals))), 
+             pos = vals/2 + lead(csum, 1),
+             pos = if_else(is.na(pos), vals/2, pos))
+    # ggplot(pie, aes(x = "", y = vals, fill = labels)) +
+    #   geom_col() +
+    #   coord_polar(theta = "y")
+    
+    # pie(pie$vals, labels=paste(round(prop.table(vals)*100), "%", sep=""), col= c("#767676", "#990000", "#FFC72C"), radius=1)
+    
+    ggplot(pie, aes(x = "", y = vals, fill = labels)) +
+      geom_col(color = "black") +
+      # ggtitle("Title") +
+      #geom_label_repel(data = pie1,
+      #                 aes(y = pos, label = paste0(vals)),
+      #                 size = 4.5, nudge_x = 1, show.legend = FALSE) +
+      geom_text(aes(label = vals),
+                position = position_stack(vjust = 0.5)) +
+      coord_polar(theta = "y") +
+      scale_fill_manual(values = c("#990000",
+                                   "#FFC72C", "#767676")) +
+      theme_void()
+  })
 
   # for find SDGs and pub by auth
   output$auth_about <- DT::renderDataTable({
